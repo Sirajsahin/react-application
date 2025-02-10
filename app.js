7eb76bdb0431f3c4af9dc46008a71c83709f9722
mac <mac@APPLEs-MacBook-Pro.local>
2025-02-10 10:19:46 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const child = React.createElement("div", { class: "h1" }, "Hey Siraj");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const child = React.createElement(
+  "div",
+  { class: "h1" },
+  "Hey Sirdscjhbdsjhbsjcsdaj"
+);
 const subparent = React.createElement("div", { class: "sub-head" }, child);
 
-const child2 = React.createElement("div", { class: "h12" }, "Hey Siraj 2");
-const subparent2 = React.createElement("div", { class: "sub-head2" }, child2);
+const child2 = React.createElement(
+  "div",
+  { class: "h12" },
+  "Hey dsbjbsjbjsdbcjsddshv  hvs 2"
+);
+const subparent2 = React.createElement(
+  "div",
+  { class: "sub-xsbsjchbdsjhcbjsdbcjds" },
+  child2
+);
 
 const parent = React.createElement("div", { class: "head" }, [
   subparent,
